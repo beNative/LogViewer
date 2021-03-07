70839e2fbf2cdc6ff38acfd2df6bd66bd2cbee9b
Tim Sinaeve <tim.sinaeve@gmail.com>
2021-03-07 15:54:07 +0100

    Added mindmap image.

--- build.bat
+++ build.bat
@@ -7,8 +7,9 @@ if %errorlevel% neq 0 (
 ) else (
   call rsvars
   if exist .\Bin\Win32\%project%.exe del .\Bin\Win32\%project%.*
-::  msbuild %project%.dproj /t:make /p:config=Release /p:platform=Win32
+  ::msbuild %project%.dproj /t:make /p:config=Release /p:platform=Win32
   msbuild %project%.dproj /t:make /p:config=Debug /p:platform=Win32
+  ::if exist .\Bin\Win64\%project%.exe del .\Bin\Win64\%project%.*
   ::msbuild %project%.dproj /t:make /p:config=Debug /p:platform=Win64
   ::msbuild %project%.dproj /t:make /p:config=Release /p:platform=Win64
   where upx.exe /q
