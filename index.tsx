29e2dfa9dc0734ff52a54b3f1be73b09f9d1c25d
Tim Sinaeve <tim.sinaeve@gmail.com>
2025-09-03 21:05:58 +0200

    feat: Update React CDN import map
    Updates the React CDN import map to version 19.1.1, ensuring the application uses the latest stable version of React.

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+// --- Original App Mounting ---
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
